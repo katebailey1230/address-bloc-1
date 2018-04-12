87462652ad901eb06334975ecef150195bb675b1
K.Bailey <katebailey1230@gmail.com>
2018-04-12 18:48:49 -0400

    adding_pg

--- address_bloc.rb
+++ address_bloc.rb
@@ -2,6 +2,7 @@ require_relative 'controllers/menu_controller'
 require 'bloc_record'
  
 BlocRecord.connect_to("db/address_bloc.sqlite")
+BlocRecord.connect_to("db/address_bloc.db", :pg)
 
 menu = MenuController.new
 system "clear"
